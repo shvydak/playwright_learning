744c1dc9c10f081065f9a5d09c99596e4e4e256f
y.shvydak <y.shvydak@gmail.com>
2023-10-16 16:44:23 +0300

    PracticeFormPage Stuck With Lombok

--- src/main/java/main_page/forms/PracticeFormPage.java
+++ src/main/java/main_page/forms/PracticeFormPage.java
@@ -3,6 +3,7 @@ package main_page.forms;
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import manager.BaseHelper;
+import models.PracticeFormModel;
 import org.testng.Assert;
 
 public class PracticeFormPage extends BaseHelper {
@@ -11,6 +12,26 @@ public class PracticeFormPage extends BaseHelper {
     }
 
     private final Locator titleText = page.locator(BaseHelper.titleLocator);
+    private final Locator firstNameInput = page.locator("id=firstName");
+    private final Locator lastNameInput = page.locator("id=lastName");
+    private final Locator emailInput = page.locator("id=userEmail");
+    private final Locator maleRadioButton = page.locator("id=gender-radio-1");
+    private final Locator femaleRadioButton = page.locator("id=gender-radio-2");
+    private final Locator otherRadioButton = page.locator("id=gender-radio-3");
+    private final Locator mobileNumber = page.locator("id=userNumber");
+    private final Locator subjects = page.locator("id=subjectsContainer");
+    private final Locator sportCheckBox = page.locator("id=hobbies-checkbox-1");
+    private final Locator readingCheckBox = page.locator("id=hobbies-checkbox-2");
+    private final Locator musicCheckBox = page.locator("id=hobbies-checkbox-3");
+    private final Locator uploadPicture = page.locator("id=uploadPicture");
+    private final Locator currentAddress = page.locator("id=currentAddress");
+    private final Locator state = page.locator("id=state");
+    private final Locator city = page.locator("id=city");
+
+    public PracticeFormPage fillPracticeForm(PracticeFormModel user) {
+        PracticeFormModel user1;
+        return this;
+    }
 
     public PracticeFormPage titleTextIs(String text) {
         Assert.assertEquals(titleText.textContent(), text);
--- src/main/java/models/PracticeFormModel.java
+++ src/main/java/models/PracticeFormModel.java
@@ -1,5 +1,14 @@
 package models;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@Builder
+@ToString
 public class PracticeFormModel {
     private String firstName;
     private String lastName;
